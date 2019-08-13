fd16f590a2888f4d8e8dc8055823275f8b451311
Frederic DUBOUCHET <frederic.dubouchet@gmail.com>
2019-08-13 17:05:14 +0200

    Initialize Winsock (and destroy after use)

--- server_main.cpp
+++ server_main.cpp
@@ -1,7 +1,21 @@
 #include <WinSock2.h>
+#include <stdio.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
-int main(int ac, char** av) {
+int main(int ac, char** av) 
+{
+	// Initialize winsock.
+	WSADATA ws_data;
+	if (WSAStartup(MAKEWORD(2, 2), &ws_data) != 0) 
+	{
+		printf("cannot initialize winsock!\n");
+		return -1;
+	}
+
+	// TODO here come the code!
+
+	// Close winsock.
+	WSACleanup();
 	return 0;
 }
